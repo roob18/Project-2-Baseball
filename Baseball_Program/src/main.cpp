3fccf37b27be75e63810ff81075a3e5b7214829d
Salomon A <94591133+salo217@users.noreply.github.com>
2025-04-24 16:06:11 -0700

    Updated main to support stylesheet and resource file

--- Baseball_Program/src/main.cpp
+++ Baseball_Program/src/main.cpp
@@ -1,10 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file(":/style.css");
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+        QString style = file.readAll();
+        a.setStyleSheet(style);
+    }
     MainWindow w;
     w.show();
     return a.exec();
-} 
+}
